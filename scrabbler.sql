90305621165c6e9bf4dfd625038edae1df72f7cf
Ankit kumar <111516810+AnkitKr0711@users.noreply.github.com>
2022-11-22 20:08:05 +0530

    Add files via upload

---
+++ scrabbler.sql
@@ -0,0 +1,34 @@
+create database scrabble;
+use scrabble;
+--- understanding the DataFrame
+
+--- Changing Datatype from string to datetime for player_games table column created_at
+
+select * from player_games;
+Alter Table player_games ALTER COLUMN created_at datetime;
+
+--- Editing table player_train making the table more comprehensive
+
+select * from player_train;
+select * from player_test;
+
+with tempo as (
+select pt1.game_id as game_id, pt1.nickname as Bot_name, pt1.score as Bot_score, pt1.rating as Bot_rating,
+pt2.nickname as player_name, pt2.score as player_score, pt2.rating as player_rating
+from player_train as pt1 join player_train as pt2 on pt1.game_id = pt2.game_id)
+select * from tempo where (Bot_name like '%Bot') and (Bot_name!=player_name)order by game_id
+
+---select *,ROW_NUMBER()over(partition by game_id order by game_id) as row_num from tempo order by game_id
+---select * from table1 where row_num =2 order by game_id
+
+--- Editing table player_test making the making the table more comprehensive
+with tempo as (
+select pt1.game_id as game_id, pt1.nickname as Bot_name, pt1.score as Bot_score, pt1.rating as Bot_rating,
+pt2.nickname as player_name, pt2.score as player_score, pt2.rating as player_rating
+from player_test as pt1 join player_test as pt2 on pt1.game_id = pt2.game_id)
+select * from tempo where (Bot_name like '%Bot') and (Bot_name!=player_name)order by game_id
+
+select * from player_turns;
+
+
+
