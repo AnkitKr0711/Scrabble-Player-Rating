0d522bc26a58e93369e0e507418b6a682edef25f
Ankit kumar <111516810+AnkitKr0711@users.noreply.github.com>
2022-11-22 20:41:34 +0530

    Update Scrabbler_SQL.sql

--- Scrabbler_SQL.sql
+++ Scrabbler_SQL.sql
@@ -12,7 +12,6 @@ Alter Table player_games ALTER COLUMN created_at datetime;
 
 select * from player_train;
 
-![sql1](https://user-images.githubusercontent.com/111516810/203348653-393c786f-ae8a-4a8b-b735-0c4df88175e9.jpg)
 
 with tempo as (
 select pt1.game_id as game_id, pt1.nickname as Bot_name, pt1.score as Bot_score, pt1.rating as Bot_rating,
@@ -20,7 +19,6 @@ pt2.nickname as player_name, pt2.score as player_score, pt2.rating as player_rat
 from player_train as pt1 join player_train as pt2 on pt1.game_id = pt2.game_id)
 select * from tempo where (Bot_name like '%Bot') and (Bot_name!=player_name)order by game_id
 
-![sql2](https://user-images.githubusercontent.com/111516810/203348921-0ed4597b-79f6-469b-bebd-78330fc118cd.jpg)
 
 --- Editing table player_test making the making the table more comprehensive
 
